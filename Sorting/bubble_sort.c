f3106ba0a76250d08450cf0ed0489193d34b5076
kunaly1038 <39200197+kunaly1038@users.noreply.github.com>
2018-07-18 12:48:58 +0530

    Create bubble_sort.c

---
+++ Sorting/bubble_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void insertArray(int [],int);
+void printArray(int [], int);
+void bubbleSort(int [],int);
+
+void main(){
+	int x;
+	int array[x];
+	printf("Enter the size of array :");
+	scanf("%d",&x);
+	insertArray( array, x);
+	bubbleSort(array,x);
+	printArray(array,x);
+}
+
+void insertArray(int array[],int x){
+	int i;
+	for(i=0;i<x;i++){
+		scanf("%d",&array[i]);
+	}
+}
+void printArray(int array[],int x){
+	int i;
+	for(i=0;i<x;i++){
+	printf("%d",array[i]);
+	}
+}
+
+void bubbleSort(int array[],int x){
+	int i,j,temp;
+	for(i=0;i<x;i++){
+		for(j=0;j<x;j++){
+			if(array[j]>array[i]){
+				temp = array[j];
+				array[j] =array[i];
+				array[i] =temp;
+			}
+		}
+	}
+}
